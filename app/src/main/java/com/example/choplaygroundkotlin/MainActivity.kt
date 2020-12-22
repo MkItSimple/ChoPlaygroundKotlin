64f2b5e03e72fce47a63e2d64a65d0f9ed5e3364
MkItSimple <choanania339@gmail.com>
2020-12-23 01:32:50 +0800

    shared-animation-between-activities

--- app/src/main/java/com/example/choplaygroundkotlin/MainActivity.kt
+++ app/src/main/java/com/example/choplaygroundkotlin/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.choplaygroundkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Pair
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        constraintLayout.setOnClickListener {
+            val intent = Intent(this, SharedActivity::class.java)
+
+            val pairs = listOf<Pair<View, String>>(
+                Pair.create<View, String>(profile_image, "imageTransition"),
+                Pair.create<View, String>(profile_name, "nameTransition"),
+                Pair.create<View, String>(profile_description, "descTransition")
+            )
+
+            val options = ActivityOptions.makeSceneTransitionAnimation(this,
+                pairs[0],
+                pairs[1],
+                pairs[2]
+            )
+
+            startActivity(intent, options.toBundle())
+        }
     }
 }
